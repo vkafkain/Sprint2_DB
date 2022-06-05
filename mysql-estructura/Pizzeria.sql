0c05dc882bdfcd4d09aa8933c6f2e3599c04ef06
Victor <vlopezmoles@gmail.com>
2022-06-05 18:38:13 +0200

    Creo script Pizzeria

--- mysql-estructura/Pizzeria.sql
+++ mysql-estructura/Pizzeria.sql
@@ -11,7 +11,7 @@ CREATE TABLE localitat (
     nom VARCHAR(255)
     provincia_id INT NOT NULL
     PRIMARY KEY(localitat)
-    FOREIGN KEY(provincia_id)
+    FOREIGN KEY(provincia_id) REFERENCES provincia(provincia_id)
 );
 CREATE TABLE client (
     client_id INT NOT NULL AUTO_INCREMENT
@@ -19,9 +19,10 @@ CREATE TABLE client (
     cognoms VARCHAR(255)
     adreça VARCHAR(255)
     telefon INT
+    codi_postal INT
     localitat_id INT NOT NULL
     PRIMARY KEY(client_id)
-    FOREIGN KEY(localitat_id)
+    FOREIGN KEY(localitat_id) REFERENCES localitat(localitat_id)
 );
 CREATE TABLE botiga(
     botiga_id INT NOT NULL AUTO_INCREMENT
@@ -29,7 +30,7 @@ CREATE TABLE botiga(
     codi_postal INT
     localitat_id INT NOT NULL
     PRIMARY KEY(botiga_id)
-    FOREIGN KEY(localitat_id)
+    FOREIGN KEY(localitat_id) REFERENCES localitat(localitat_id)
 );
 CREATE TABLE empleat(
     empleat_id INT NOT NULL AUTO_INCREMENT
@@ -37,17 +38,56 @@ CREATE TABLE empleat(
     cognoms VARCHAR(255)
     NIF VARCHAR(255)
     telefon INT
-    botiga INT NOT NULL
+    tipus_treballador ENUM('repartidor', 'cuiner')
+    botiga_id INT NOT NULL
     PRIMARY KEY(empleat_id)
-    FOREIGN KEY(botiga_id)
+    FOREIGN KEY(botiga_id) REFERENCES botiga(botiga_id)
 );
 CREATE TABLE comanda_domicili (
     comanda_domicili_id INT NOT NULL AUTO_INCREMENT
-    data_entrega DATE
-    client_id INT NOT NULL
+    empleat_id INT NOT NULL
+    comanda_id
     PRIMARY KEY(comanda_domicili_id)
-    FOREIGN KEY(client_id)
+    FOREIGN KEY(empleat_id) REFERENCES empleat_id(empleat_id)
 );
-)
-
-
+CREATE TABLE comanda (
+    comanda_id INT NOT NULL AUTO_INCREMENT
+    data DATE
+    hora DATE TIME
+    tipus_comanda ENUM('domicili', 'botiga')
+    preu_total INT  
+    client_id INT NOT NULL
+    botiga_id INT NOT NULL
+    empleat_id INT NOT NULL
+    comanda_domicili_id INT 
+    PRIMARY KEY(comanda_id)
+    FOREIGN KEY(client_id) REFERENCES client(client_id)
+    FOREIGN KEY(botiga_id) REFERENCES botiga(botiga_id)
+    FOREIGN KEY(empleat_id) REFERENCES empleat(empleat_id)
+    FOREIGN KEY(comanda_domicili_id) REFERENCES comanda_domicili(comanda_domicili_id)
+);
+CREATE TABLE categoria_pizzes (
+    categoria_pizzes_id INT NOT NULL AUTO_INCREMENT
+    nom VARCHAR(255)
+    PRIMARY KEY(categoria_pizzes_id)
+);
+CREATE TABLE productes (
+    productes_id INT NOT NULL AUTO_INCREMENT
+    tipus_producte ENUM ('pizza', 'hamburguesa', 'beguda')
+    nom VARCHAR(255)
+    descripcio VARCHAR(255)
+    imatge BLOB
+    preu INT
+    categoria_pizzes_id INT NOT NULL
+    PRIMARY KEY(productes_id)
+    FOREIGN KEY(categoria_pizzes_id) categoria_pizzes(categoria_pizzes_id)
+);
+CREATE TABLE quantitat_producte (
+    quantitat_producte_id INT NOT NULL AUTO_INCREMENT
+    quantitat INT
+    producte_id INT NOT NULL
+    comanda_id INT NOT NULL
+    PRIMARY KEY(quantitat_producte_id)
+    FOREIGN KEY(producte_id) producte(producte_id)
+    FOREIGN KEY(comanda_id) comanda(producte_id)
+);
