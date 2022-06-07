ca17bd7c242e224cf7b65cf75be0925193c6cff0
Victor <vlopezmoles@gmail.com>
2022-06-07 12:41:02 +0200

    actualitzo queries_tienda

--- mysql-queries/queries_tienda.sql
+++ mysql-queries/queries_tienda.sql
@@ -6,3 +6,4 @@ SELECT nombre, precio, precio * 1.07 FROM producto;
 SELECT nombre AS 'nom de producto', precio AS euros, precio * 1.07 AS dolars FROM producto;
 SELECT UPPER(nombre), precio FROM producto;
 SELECT LOWER(nombre), precio FROM producto;
+SELECT nombre, UPPER(SUBSTR(nombre, 1, 2)) FROM fabricante;
