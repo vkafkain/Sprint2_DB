82ab5fa02f0330388fab32a413d041e0738851f5
Victor <vlopezmoles@gmail.com>
2022-06-07 12:35:34 +0200

    queries_tienda init

---
+++ mysql-queries/queries_tienda.sql
@@ -0,0 +1,8 @@
+USE tienda;
+SELECT nombre FROM producto;
+SELECT nombre, precio FROM producto;
+SELECT * FROM producto;
+SELECT nombre, precio, precio * 1.07 FROM producto;
+SELECT nombre AS 'nom de producto', precio AS euros, precio * 1.07 AS dolars FROM producto;
+SELECT UPPER(nombre), precio FROM producto;
+SELECT LOWER(nombre), precio FROM producto;
