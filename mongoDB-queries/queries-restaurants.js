818a5ef5f45134fae278cf3da571367bc6555107
Victor <vlopezmoles@gmail.com>
2022-06-22 16:00:31 +0200

    Actualitzo mongoDB-queries

--- mongoDB-queries/queries-restaurants.js
+++ mongoDB-queries/queries-restaurants.js
@@ -1,2 +1,26 @@
-db.restaurants.find({}, {name: 1, borough: 1, cuisine: 1})
-
+db.restaurants.find();
+db.restaurants.find({}, {name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({}, {_id: 0, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({}, {_id: 0, name: 1, borough: 1, 'address.zipcode': 1});
+db.restaurants.find({borough: "Bronx"});
+db.restaurants.find({borough: "Bronx"}).limit(5);
+db.restaurants.find({borough: "Bronx"}).limit(5).skip(5);
+db.restaurants.find({ "grades.score": { $gte: 90 } });
+db.restaurants.find(({grades: {$elemMatch:{score: {$gt:80, $lt:100}}}}));
+db.restaurants.find({ "address.coord": { $lt: -95.754168 } });
+db.restaurants.find({$and:[ {"cuisine" : {$ne :"American "}}, {"grades.score" : {$gt : 70}}, {"address.coord" : {$lt : -65.754168}} ]});
+db.restaurants.find({"cuisine" : {$ne : "American "},"grades.score" :{$gt: 70}, "address.coord" : {$lt : -65.754168} });
+db.restaurants.find({"cuisine" : {$ne :"American"}, "grades.grade" : "A", "borough" : {$ne : "Brooklyn"}}).sort( { "cuisine" : -1 } );
+db.restaurants.find({name: /^Wil/},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.restaurants.find({name: /ces$/},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.restaurants.find({name: /Reg/},{restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.restaurants.find({borough: "Bronx", cuisine: /(American|Chinese)/}, {borough:1, cuisine:1, _id:0});
+db.restaurants.find({borough: /(State Island|Queens|Bronx|Brooklyn)/}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0})
+db.restaurant.find({borough: {$nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.restaurants.find({"grades.score": { $lte: 10 }}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+db.restaurants.find({$and: [{cuisine: { $ne: "American " }}, {cuisine: { $ne: "Chinese" }}, {$or:[{name: {$regex: /fish/i}},{name: {$regex: /^Wil/i}}]} ] }, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+db.restaurants.find({ grades: {$elemMatch: { score: 11, date: ISODate("2014-08-11T00:00:00Z") }}, "grades.grade": "A" }, { restaurant_id: 1, name: 1, grades: 1 });
+db.restaurants.find({ "grades.1.score": 9, "grades.1.date": ISODate("2014-08-11T00:00:00Z"), "grades.1.grade": "A" }, { restaurant_id: 1, name: 1, grades: 1 });
+db.restaurants.find({$and:[{"address.coord.1":{$gt:42}}, {"address.coord.1":{$lte:52}}]}, {_id:0, restaurant_id: 1, name: 1, address: 1 });
+db.restaurants.find().sort( {name: 1 } );
+db.restaurants.find().sort( {name: -1 } );
